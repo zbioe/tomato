ddd0ee8174c9e9ba57f4b457b2a49e5be701ba85
Iury Fukuda <zbioe@protonmail.com>
2020-05-11 19:53:22 -0300

    add structure of script

--- tomato.sh
+++ tomato.sh
@@ -6,18 +6,21 @@ set -o nounset
 usage() {
   cat <<EOF
 Usage of $0:
-  $0 [option] [message]
+  $0 [option]
 
 Options:
-  -h, --help                     Show this message
-  -w, --work-session <duration>  Duration of a work session (Default 25m)
-  -s, --short-break <duration>   Duration of a short break (Default 5m)
-  -l, --long-beak <duration>     Duration of a long break (Default 15m)
-  -e, --cmd <string>             Command to execute when session is done
+  -h, --help               Show this message
+  -w, --work <duration>    Duration of a work session (Default 25m)
+  -b, --break <duration>   Duration of a short break (Default 5m)
+  -c, --cicles <number>    Number of Cicles (Default 4)
+  -n, --notifyer <script>  Script to use as notifyer (Default notify-send)
 
 Examples:
-  $0 -w 2m -s 30s -l 1m -e "notify-send 'end task'"
-  $0 -e "/bin/custom_end_task.sh"
+  $0 -w 2m -b 30s -c 6
+  $0 -n "/bin/custom_notifyer.sh"
+
+Notifyer:
+  notify [message]
 EOF
 }
 
@@ -27,24 +30,35 @@ err() {
   exit 1
 }
 
+noise() {
+  (
+    speaker-test -l 1 -p 1 -t sine &
+  ) 1>/dev/null 2>&1
+}
+
+notify() {
+  notify-send "$@"
+  which speaker-test 1>/dev/null 2>&1 && noise
+}
+
 while [ $# -gt 0 ] ; do
-  nSkip=1
+  nSkip=2
   case $1 in
     "-h"|"--help")
       usage
       exit 0
       ;;
-    "--work-session"|"-w")
-      nSkip=2
+    "--work"|"-w")
+      ws=$2
       ;;
-    "--short-break"|"-s")
-      nSkip=2
+    "--break"|"-b")
+      b=$2
       ;;
-    "--long-break"|"-l")
-      nSkip=2
+    "--cicles"|"-c")
+      c=$2
       ;;
-    "-e"|"--cmd")
-      nSkip=2
+    "--notifyer"|"-n")
+      n=$2
       ;;
     *)
       err "invalid option"
@@ -52,3 +66,25 @@ while [ $# -gt 0 ] ; do
   esac
   shift $nSkip
 done
+
+default_w="25m"
+default_b="5m"
+default_c="4"
+default_n=notify
+
+W=${w-$default_w}
+B=${b-$default_b}
+C=${c-$default_c}
+N=${n-$default_n}
+
+while [ $C -ge 0 ]
+do
+  $N "focus mode"
+  sleep $W
+  $N "relax mode"
+  sleep $B
+  C=$(expr $C - 1)
+done
+
+exit 0
+
